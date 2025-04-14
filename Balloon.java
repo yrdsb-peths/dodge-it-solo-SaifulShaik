f1dc769c7cca30446686148dfe9eea06d0f54441
Saiful Shaik <Saifulshaik47@gmail.com>
2025-04-14 14:28:41 -0400

    Balloon loop

--- Balloon.java
+++ Balloon.java
@@ -12,9 +12,18 @@ public class Balloon extends Actor
      * Act - do whatever the Balloon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
         // Add your action code here.
         move(3);
+        
+        if(getX() >= 599) {
+            resetBalloon();
+        }
+    }
+    
+    public void resetBalloon() {
+        setLocation(0, 200);
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -3,10 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        int height = getHeight();
+        int width = getWidth();
         Hero hero = new Hero();
         addObject(hero, 100, 100);
         
         Balloon ball = new Balloon();
-        addObject(ball, 0, getHeight()/2);
+        addObject(ball, 0, height/2);
     }
 }
